e4507c53dc4b3ed0cb2cc7396ea2695d776185ae
rickizzisj <roqueehs@gmail.com>
2025-09-05 22:11:07 -0300

    commit 9/5

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,7 +43,28 @@ public class LivroController {
             model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
             return "livroCadastro";
         }
-        livroService.createLivro(livro);
+        if (livro.getId() == null) {
+            livroService.createLivro(livro);
+        } else {
+            livroService.updateLivro(livro);
+        }
+        return listarLivros(model);
+    }
+
+    @GetMapping("/cadastro/{id}")
+    public String cadastroLivro(@PathVariable Long id, Model model) {
+        Livro livro = livroService.readLivro(id);
+        if (livro == null) {
+            listarLivros(model);
+        }
+        model.addAttribute("livro", livro);
+        model.addAttribute("categoriaLista", Arrays.asList(Categoria.values()));
+        return "livroCadastro";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarLivro(@PathVariable Long id, Model model) {
+        livroService.deleteLivro(id);
         return listarLivros(model);
     }
 }
